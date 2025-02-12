3f761af0d41a70c726bd6f911ee59bab1fa4c0d9
Adriano Neto <adrianoneto27@gmail.com>
2025-02-12 13:47:33 -0300

    Update weather_dag2.py

--- weather_dag2.py
+++ weather_dag2.py
@@ -4,7 +4,7 @@ from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
 from datetime import datetime
 
-API_KEY = "3bd0125aa8cd0eada756ea251e8b2aa6"
+API_KEY = "API_KEY"
 CITY = "São Paulo"
 
 # Configurações do PostgreSQL
